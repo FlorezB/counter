0b06f8640eeb665c411d79e52b183b64fe441db2
Caroline <alyseflorez@hotmail.fr>
2022-07-12 15:15:30 +0200

    exo compteur1 done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+class App extends React.Component {
+  
+  constructor () {
+    super ()
+
+    this.state = {
+      count: 0
+    }
+  }
+
+  handleClick = () =>
+
+  this.setState({
+    count: this.state.count + 1
+    // this.count
+  })
+
+
+  handleClick2 = () =>
+
+  this.setState({
+    count: this.state.count - 1
+    // this.count
+  })
+
+
+  handleChange = e => {
+    console.log(e.target)
+    const inputValue = e.target.value
+    
+    this.setState({
+      count: inputValue
+    })
+  }
+
+  render () {
+    return (
+      <div className="container">
+        <h1 className="mb-3">Counter</h1>
+        <h2>
+          {this.state.count}
+        </h2>
+        <button className ="btn btn-outline-success" onClick={this.handleClick2}> - </button>
+        <button className ="btn btn-outline-danger" onClick={this.handleClick}> + </button>
+      </div>
+    )
+  }
 }
 
 export default App;
